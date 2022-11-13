bb62446cd0146bdbf10bee385fdbffc2f7111fb3
Jens Grochtdreis <jens@grochtdreis.de>
2022-11-13 23:43:12 +0100

    gulp works now

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,55 @@
-// Basis
-// https://github.com/philhawksworth/hawksworx.com/blob/master/gulpfile.js
-
-const gulp    = require("gulp");
+let { src, dest } = require('gulp');
 const sass    = require("gulp-sass")(require("sass"));
 
+// Styles
+const postcss = require('gulp-postcss');
+const prefix = require('autoprefixer');
+const minify = require('cssnano');
+const rename = require('gulp-rename');
+
+/**
+ * Paths to project folders
+ */
+
+ var paths = {
+	input: 'src/',
+	output: '_site/',
+	scripts: {
+		input: 'src/assets/js/*',
+		output: '_site/js/'
+	},
+	styles: {
+		input: 'src/assets/scss/**/*.scss',
+		output: '_site/css/',
+    mainFile: 'src/assets/scss/main.scss'
+	}
+};
+
+
 /* ==== generate the css with sass */
-gulp.task('css', function() {
-  return gulp.src('scss/main.scss')
-    .pipe(sass({
-      outputStyle: 'expanded'
-     // outputStyle: 'compressed'
+
+function css(){
+  return src(paths.styles.mainFile)
+  .pipe(sass({
+    outputStyle: "expanded",
+    sourceComments: true
+  }))
+  .pipe(postcss([
+    prefix({
+      cascade: true,
+      remove: true
     })
-    .on('error', sass.logError))
-    .pipe(gulp.dest('_site/css'));
-});
+  ]))
+  .pipe(dest(paths.styles.output))
+  .pipe(rename({suffix: ".min"}))
+  .pipe(postcss([
+    minify({
+      discardComments: {
+        removeAll: true
+      }
+    })
+  ]))
+  .pipe(dest(paths.styles.output));
+}
 
-/* === Watch folders for changes */
-gulp.task("watch", function() {
-  gulp.watch('./scss/**/*.scss', gulp.parallel('css'));
-});
-
-/* === Let's build this sucker. */
-gulp.task('build', gulp.parallel(
-  'css'
-));
+exports.css = css;
