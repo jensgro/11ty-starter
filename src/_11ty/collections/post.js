e8fb72b421dc964ea904dff9c4b2dca5b7d8181d
Jens Grochtdreis <jens@grochtdreis.de>
2022-11-18 23:53:03 +0100

    collections korrigiert

--- src/_11ty/collections/post.js
+++ src/_11ty/collections/post.js
@@ -1,4 +1,4 @@
 module.exports = function(collection) {
   return collection
-    .getFilteredByGlob("./src/content/post/*.md")
+    .getFilteredByGlob(["./src/content/post/*.md", "./src/content/post/*.njk"])
 }
--- src/_11ty/collections/postDescending.js
+++ src/_11ty/collections/postDescending.js
@@ -4,7 +4,7 @@
 // https://github.com/11ty/eleventy/issues/411
 module.exports = function(collection) {
   return collection
-    .getFilteredByGlob("./src/content/post/*.md").sort((a,b) => {
+    .getFilteredByGlob(["./src/content/post/*.md", "./src/content/post/*.njk"]).sort((a,b) => {
       if (a.data.title < b.data.title) return -1;
       else if (a.data.title > b.data.title) return 1;
       else return 0;
